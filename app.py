81ed3134048ac3b5ba730b294ef2ca4ab0f75785
KidMongoose <kidmongoose@gmail.com>
2023-05-11 13:34:54 +0700

    CSRF protection, email invitation template and route added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 socket = SocketIO(app)
+csrf = CSRFProtect(app)
 load_dotenv('.env')
 
 
@@ -17,6 +19,10 @@ def chat():
 
 @app.route('/chatroom/<str:chatroom>')
 def chatroom():
+    pass
+
+@app.route('/invitation')
+def invitation():
     pass
 
 if __name__ == '__main__':
